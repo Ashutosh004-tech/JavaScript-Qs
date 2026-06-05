253e03b4f21e793813e91460baa7f64d91bc5d0a
Ashutosh Panigrahi <ashutoshpanigrahi11@gmail.com>
2026-06-05 10:48:35 +0530

    Variables

--- Basics/01_variables.js
+++ Basics/01_variables.js
@@ -1,7 +1,16 @@
+//Declares a block-scoped, Must be initialize and can't change once in the programm execution .
 const name = "John"
+//Declares a block-scoped, local variable, optionally initializing it to a value.
 let age = 20
+//Declares a variable, optionally initializing it to a value.(Not prefer for use)
 var password = "12345678"
-
+//
 email = "john2@gmail.com"
 
-console.log(name);
+const i =9
+// i++ Not allowed for a constant variable 
+
+console.log("Output:-");
+
+
+console.table([name, age, password, email ]);
