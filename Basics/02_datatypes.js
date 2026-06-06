8bdad2d3df2d1248aa5d99c8832a8d2253337a27
Ashutosh Panigrahi <ashutoshpanigrahi11@gmail.com>
2026-06-06 19:25:15 +0530

    2nd part

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+// 1.Number
+    //Number can store integers in the range -(2^53 − 1) to 2^53 − 1
+ let a = 100
+
+ // 2.String
+let name = "John"
+
+// 3.Boolean
+let isOkay = true
+
+// 4.Undefined 
+let val;
+
+// 5.Null
+let data = null
+
+// 6.Symbol
+// 7.BigInt
+
+console.log(typeof(a)) //  Number
+
+console.log(typeof(data)) // Object
+
+console.log(typeof(val)) // Undefined 
+
+console.log(typeof(isOkay))  // Boolean
+
+console.log(typeof(name)) // String
