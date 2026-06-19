7b4e084f2d6099d43df8d8a0ea75dd6b402bae12
Ashutosh Panigrahi <ashutoshpanigrahi11@gmail.com>
2026-06-19 11:30:20 +0530

    JavaScript Fundamentals

--- Objects/04_ObjectMethods.js
+++ Objects/04_ObjectMethods.js
@@ -1,5 +1,34 @@
 let Emp = {
     empId : "emp102",
     Empname: "Rk",
-    
+    EmpField : "Manager",
+    salary: 20000
 }
+
+console.log(Emp.hasOwnProperty("salary")); // True
+
+console.log(Object.entries(Emp))
+/*
+[
+  [ 'empId', 'emp102' ],
+  [ 'Empname', 'Rk' ],
+  [ 'EmpField', 'Manager' ],
+  [ 'salary', 20000 ]
+]
+ */
+
+console.log(Object.keys(Emp)); // [ 'empId', 'Empname', 'EmpField', 'salary' ]
+
+console.log(Object.values(Emp)); // [ 'emp102', 'Rk', 'Manager', 20000 ]
+
+console.log(Object.assign(Emp,{vlg:"chicago", con : "USA"}));
+/*
+{
+  empId: 'emp102',
+  Empname: 'Rk',
+  EmpField: 'Manager',
+  salary: 20000,
+  vlg: 'chicago',
+  con: 'USA'
+}
+*/
