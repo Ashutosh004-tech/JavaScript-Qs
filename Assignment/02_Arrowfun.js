2db864ab098665374ccf1bcdf74b7828f4465129
Ashutosh Panigrahi <ashutoshpanigrahi11@gmail.com>
2026-06-27 09:50:35 +0530

    Square, Even and Largest Number

---
+++ Assignment/02_Arrowfun.js
@@ -0,0 +1,12 @@
+//Square of a number
+const square = (num) => num * num;
+console.log(square(6));
+
+//check if anumber is even
+const isEven = (num) => num % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(5));
+
+//fid the largest of two numbers
+const max = (a, b) => (a > b ? a : b);
+console.log(max(15, 18));
