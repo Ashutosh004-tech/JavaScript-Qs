f67cbf5a41cd945a89004a88fb8384f2ab3eccbb
Ashutosh Panigrahi <ashutoshpanigrahi11@gmail.com>
2026-06-27 09:49:15 +0530

    Arrow function in js

--- Function/04_arrowFunction.js
+++ Function/04_arrowFunction.js
@@ -29,16 +29,5 @@ const val = a => a + 10;
 
 console.log(val(3));
 
-//Square of a number 
-const square=num=>num*num;
-console.log(square(6));
 
-//check if anumber is even 
-const isEven=num=>num%2===0
-console.log(isEven(8));
-console.log(isEven(5));
-
-//fid the largest of two numbers
-const max=(a,b)=>(a>b?a:b)
-console.log(max(15,18));
 
