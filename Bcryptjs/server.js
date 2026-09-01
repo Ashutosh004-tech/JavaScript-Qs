cc65209657b7d4f008ac6d7c06b516fa241497ab
Ashutosh Panigrahi <ashutoshpanigrahi11@gmail.com>
2026-09-01 07:50:39 +0530

    Formatting Server

--- Bcryptjs/server.js
+++ Bcryptjs/server.js
@@ -14,15 +14,19 @@ app.post("/user/new", async (req, res) => {
   try {
     const { name, email, password } = req.body;
     let userExist = await User.findOne({ email });
+
     if (userExist) {
-     return res.status(400).json({msg:"User Exist"});
+      return res.status(400).json({ msg: "User Exist" });
     }
+
     const hashPassword = await bcrypt.hash(password, 10);
     const Res = await User.create({ name, email, password: hashPassword });
-    res.status(200).json({Res});
+    res.status(200).json({ Res });
+
   } catch (er) {
     console.log(er);
   }
+  
 });
 
 app.listen(port, () => {
